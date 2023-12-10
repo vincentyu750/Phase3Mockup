cdc7e6cb0f99f963ed181367a48501d188eaf8ac
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-12-10 12:56:06 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,46 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const RegisterModel = require('./models/Register');
 
-const app = express()
+const app = express();
+
+// Use specific origin instead of "*"
 app.use(cors({
-    origin: "*",
-    methods: ["POST", "GET"],
-    credentials: true
-  }));
+  origin: "https://your-frontend-app.vercel.app",
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
-app.use(express.json())
+app.use(express.json());
 
 mongoose.connect('mongodb+srv://vincent:123@employee.yvqm0ok.mongodb.net/test?retryWrites=true&w=majority');
 
-
 app.get("/", (req, res) => {
-    res.json("Sucessful");
-})
+  res.json("Successful");
+});
+
 app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
-
-    RegisterModel.findOne({email: email})
+  RegisterModel.findOne({ email: email })
     .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-})
+      if (user) {
+        // Use 400 status for client errors
+        res.status(400).json("Already have an account");
+      } else {
+        RegisterModel.create({ name: name, email: email, password: password })
+          .then(result => res.status(201).json(result))
+          .catch(err => res.status(500).json(err));
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-//Debug
+// Debug
 app.use((req, res, next) => {
-    console.log(`Incoming request to: ${req.url}`);
-    next();
-  });
-  
+  console.log(`Incoming request to: ${req.url}`);
+  next();
+});
 
-app.listen(3001, () => {
-    console.log("Server is Running")
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
