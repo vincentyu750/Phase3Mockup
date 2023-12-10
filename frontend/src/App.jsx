82860f781f93fe28018780ae301378062c895b97
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-12-10 11:56:07 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,6 @@ axios.defaults.withCredentials = true;
           <button type="submit" className="btn btn-success w-100 rounded-0">
             Register
           </button>
-          <p>Already Have an Account</p>
           <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
             Login
           </button>
